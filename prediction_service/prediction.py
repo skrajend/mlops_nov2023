525672848d54b0a64d4e565dd6368a1fd8c04207
skrajend <rsenthilkumar.in@gmail.com>
2023-12-01 12:20:53 +0530

    commit

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -38,7 +38,7 @@ def predict(data) :
 def api_response(dict_request) :
     try:
         if validate_input(dict_request) :
-            data = np.array([list(request.json.values())])
+            data = np.array([list(dict_request.json.values())])
             response = predict(data)
             response = {"response":response}
             return response
